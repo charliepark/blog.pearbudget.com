8b108c908b72f078d3431d102cbf8f86388e9f16
Charlie Park <charlie@pearbudget.com>
2011-05-30 07:55:34 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# The PearBudget Blog
+## Code Repository
+
+Since WordPress doesn't have a great means of handling version control, this is a place where I can stash the blog's code as we develop it. Rollbacks won't be quite as granular as git normally allows, but I hope to paste in code snapshots as we go, in case I need to revert to earlier versions.
+
+The blog itself is at http://blog.pearbudget.com/.
---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+
+	?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="header">
+				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
+				<<?php echo $heading_tag; ?> id="site-title">
+						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				</<?php echo $heading_tag; ?>>
+				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+
+				<?php
+					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
+							has_post_thumbnail( $post->ID ) &&
+							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
+							$image[1] >= HEADER_IMAGE_WIDTH ) :
+						// Houston, we have a new header image!
+						echo get_the_post_thumbnail( $post->ID );
+					elseif ( get_header_image() ) : ?>
+						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
+					<?php endif; ?>
+	</div><!-- #header -->
+
+	<div id="main">
+
+
+		<div id="container">
+			<div id="content" role="main">
+
+			<?php /* Display navigation to next/previous pages when applicable */ ?>
+			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+				<div id="nav-above" class="navigation">
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+				</div><!-- #nav-above -->
+			<?php endif; ?>
+
+			<?php /* If there are no posts to display, such as an empty archive page */ ?>
+			<?php if ( ! have_posts() ) : ?>
+				<div id="post-0" class="post error404 not-found">
+					<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-0 -->
+			<?php endif; ?>
+
+			<?php
+				/* Start the Loop.
+				 *
+				 * In Twenty Ten we use the same loop in multiple contexts.
+				 * It is broken into three main parts: when we're displaying
+				 * posts that are in the gallery category, when we're displaying
+				 * posts in the asides category, and finally all other posts.
+				 *
+				 * Additionally, we sometimes check for whether we are on an
+				 * archive page, a search page, etc., allowing for small differences
+				 * in the loop on each template without actually duplicating
+				 * the rest of the loop that is shared.
+				 *
+				 * Without further ado, the loop:
+				 */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php /* How to display posts of the Gallery format. The gallery category is the old way. */ ?>
+
+				<?php if ( ( function_exists( 'get_post_format' ) && 'gallery' == get_post_format( $post->ID ) ) || in_category( _x( 'gallery', 'gallery category slug', 'twentyten' ) ) ) : ?>
+					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+						<div class="entry-meta">
+							<?php twentyten_posted_on(); ?>
+						</div><!-- .entry-meta -->
+
+						<div class="entry-content">
+			<?php if ( post_password_required() ) : ?>
+							<?php the_content(); ?>
+			<?php else : ?>
+							<?php
+								$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
+								if ( $images ) :
+									$total_images = count( $images );
+									$image = array_shift( $images );
+									$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
+							?>
+									<div class="gallery-thumb">
+										<a class="size-thumbnail" href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
+									</div><!-- .gallery-thumb -->
+									<p><em><?php printf( _n( 'This gallery contains <a %1$s>%2$s photo</a>.', 'This gallery contains <a %1$s>%2$s photos</a>.', $total_images, 'twentyten' ),
+											'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
+											number_format_i18n( $total_images )
+										); ?></em></p>
+							<?php endif; ?>
+									<?php the_excerpt(); ?>
+			<?php endif; ?>
+						</div><!-- .entry-content -->
+
+						<div class="entry-utility">
+						<?php if ( function_exists( 'get_post_format' ) && 'gallery' == get_post_format( $post->ID ) ) : ?>
+							<a href="<?php echo get_post_format_link( 'gallery' ); ?>" title="<?php esc_attr_e( 'View Galleries', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
+							<span class="meta-sep">|</span>
+						<?php elseif ( in_category( _x( 'gallery', 'gallery category slug', 'twentyten' ) ) ) : ?>
+							<a href="<?php echo get_term_link( _x( 'gallery', 'gallery category slug', 'twentyten' ), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
+							<span class="meta-sep">|</span>
+						<?php endif; ?>
+							<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-utility -->
+					</div><!-- #post-## -->
+
+			<?php /* How to display posts of the Aside format. The asides category is the old way. */ ?>
+
+				<?php elseif ( ( function_exists( 'get_post_format' ) && 'aside' == get_post_format( $post->ID ) ) || in_category( _x( 'asides', 'asides category slug', 'twentyten' ) )  ) : ?>
+					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
+						<div class="entry-summary">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-summary -->
+					<?php else : ?>
+						<div class="entry-content">
+							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
+						</div><!-- .entry-content -->
+					<?php endif; ?>
+
+						<div class="entry-utility">
+							<?php twentyten_posted_on(); ?>
+							<span class="meta-sep">|</span>
+							<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-utility -->
+					</div><!-- #post-## -->
+
+			<?php /* How to display all other posts. */ ?>
+
+				<?php else : ?>
+					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php $post_heading_tag = ( is_single() ) ? 'h1' : 'h2'; ?>
+					<<?php echo $post_heading_tag; ?> class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></<?php echo $post_heading_tag; ?>>
+
+						<div class="entry-meta">
+							<?php
+							printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'twentyten' ),
+								'meta-prep meta-prep-author',
+								sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+									get_permalink(),
+									esc_attr( get_the_time() ),
+									get_the_date()
+								),
+								sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+									get_author_posts_url( get_the_author_meta( 'ID' ) ),
+									sprintf( esc_attr__( 'View all posts by %s', 'twentyten' ), get_the_author() ),
+									get_the_author()
+								)
+							);
+							?>
+						</div><!-- .entry-meta -->
+
+				<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
+						<div class="entry-summary">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-summary -->
+				<?php else : ?>
+						<div class="entry-content">
+							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
+							<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						</div><!-- .entry-content -->
+				<?php endif; ?>
+
+						<div class="entry-utility">
+							<?php if ( count( get_the_category() ) ) : ?>
+								<span class="cat-links">
+									<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+								</span>
+								<span class="meta-sep">|</span>
+							<?php endif; ?>
+							<?php
+								$tags_list = get_the_tag_list( '', ', ' );
+								if ( $tags_list ):
+							?>
+								<span class="tag-links">
+									<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+								</span>
+								<span class="meta-sep">|</span>
+							<?php endif; ?>
+							<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-utility -->
+					</div><!-- #post-## -->
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
+
+			<?php endwhile; // End the loop. Whew. ?>
+
+			<?php /* Display navigation to next/previous pages when applicable */ ?>
+			<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+							<div id="nav-below" class="navigation">
+								<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+								<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+							</div><!-- #nav-below -->
+			<?php endif; ?>
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+				<div id="primary" class="widget-area" role="complementary">
+					<ul class="xoxo">
+
+		<?php
+			/* When we call the dynamic_sidebar() function, it'll spit out
+			 * the widgets for that widget area. If it instead returns false,
+			 * then the sidebar simply doesn't exist, so we'll hard-code in
+			 * some default sidebar stuff just in case.
+			 */
+			if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+
+					<li id="search" class="widget-container widget_search">
+						<?php get_search_form(); ?>
+					</li>
+
+					<li id="archives" class="widget-container">
+						<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
+						<ul>
+							<?php wp_get_archives( 'type=monthly' ); ?>
+						</ul>
+					</li>
+
+					<li id="meta" class="widget-container">
+						<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
+						<ul>
+							<?php wp_register(); ?>
+							<li><?php wp_loginout(); ?></li>
+							<?php wp_meta(); ?>
+						</ul>
+					</li>
+
+				<?php endif; // end primary widget area ?>
+					</ul>
+				</div><!-- #primary .widget-area -->
+
+		<?php
+			// A second sidebar for widgets, just because.
+			if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
+
+				<div id="secondary" class="widget-area" role="complementary">
+					<ul class="xoxo">
+						<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+					</ul>
+				</div><!-- #secondary .widget-area -->
+
+		<?php endif; ?>
+
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+	</div><!-- #footer -->
+
+
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
