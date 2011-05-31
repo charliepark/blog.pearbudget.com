920493bc1417e81616adb29e353acb32610a7c0b
Charlie Park <charlie@pearbudget.com>
2011-05-30 20:49:54 -0400

    Latest design and CSS.

--- index.php
+++ index.php
@@ -42,49 +42,18 @@
 ?>
 </head>
 
-<body <?php body_class(); ?>>
-	<div id="header">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</<?php echo $heading_tag; ?>>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-
-				<?php
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID );
-					elseif ( get_header_image() ) : ?>
-						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-					<?php endif; ?>
-	</div><!-- #header -->
-
-	<div id="main">
-
-
-		<div id="container">
-			<div id="content" role="main">
-
-			<?php /* Display navigation to next/previous pages when applicable */ ?>
-			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-above" class="navigation">
-					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-				</div><!-- #nav-above -->
-			<?php endif; ?>
+<body <?php body_class('cf'); ?>>
+	<?php $page_heading_tag = ( is_single() ) ? 'h2' : 'h1'; ?>
+	<<?php echo $page_heading_tag; ?> class="g gfl gc5 page_title"  style="background:#191919;margin-top:72px;margin-bottom:43px;"><a href="/">PearBudget <span>is a really simple budgeting tool. This is the PearBudget blog.</span></a></<?php echo $page_heading_tag; ?>>
+	<form action="http://blog.pearbudget.com/" id="search-form" method="get" role="search"><input type="text" id="s" name="s" placeholder="Search"></form>
+			<div id="content" class="g gfl gc3 cf">
 
 			<?php /* If there are no posts to display, such as an empty archive page */ ?>
 			<?php if ( ! have_posts() ) : ?>
-				<div id="post-0" class="post error404 not-found">
-					<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
+				<div id="post-0" class="post cf error404 not-found">
+					<h1>Not Found</h1>
+						<p>Sadly, we couldn&rsquo;t find any results for that request. You might try searching again.</p>
 						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
 				</div><!-- #post-0 -->
 			<?php endif; ?>
 
@@ -105,82 +74,11 @@
 				 */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* How to display posts of the Gallery format. The gallery category is the old way. */ ?>
-
-				<?php if ( ( function_exists( 'get_post_format' ) && 'gallery' == get_post_format( $post->ID ) ) || in_category( _x( 'gallery', 'gallery category slug', 'twentyten' ) ) ) : ?>
-					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-
-						<div class="entry-meta">
-							<?php twentyten_posted_on(); ?>
-						</div><!-- .entry-meta -->
-
-						<div class="entry-content">
-			<?php if ( post_password_required() ) : ?>
-							<?php the_content(); ?>
-			<?php else : ?>
-							<?php
-								$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
-								if ( $images ) :
-									$total_images = count( $images );
-									$image = array_shift( $images );
-									$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
-							?>
-									<div class="gallery-thumb">
-										<a class="size-thumbnail" href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
-									</div><!-- .gallery-thumb -->
-									<p><em><?php printf( _n( 'This gallery contains <a %1$s>%2$s photo</a>.', 'This gallery contains <a %1$s>%2$s photos</a>.', $total_images, 'twentyten' ),
-											'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
-											number_format_i18n( $total_images )
-										); ?></em></p>
-							<?php endif; ?>
-									<?php the_excerpt(); ?>
-			<?php endif; ?>
-						</div><!-- .entry-content -->
-
-						<div class="entry-utility">
-						<?php if ( function_exists( 'get_post_format' ) && 'gallery' == get_post_format( $post->ID ) ) : ?>
-							<a href="<?php echo get_post_format_link( 'gallery' ); ?>" title="<?php esc_attr_e( 'View Galleries', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
-							<span class="meta-sep">|</span>
-						<?php elseif ( in_category( _x( 'gallery', 'gallery category slug', 'twentyten' ) ) ) : ?>
-							<a href="<?php echo get_term_link( _x( 'gallery', 'gallery category slug', 'twentyten' ), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
-							<span class="meta-sep">|</span>
-						<?php endif; ?>
-							<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-						</div><!-- .entry-utility -->
-					</div><!-- #post-## -->
-
-			<?php /* How to display posts of the Aside format. The asides category is the old way. */ ?>
-
-				<?php elseif ( ( function_exists( 'get_post_format' ) && 'aside' == get_post_format( $post->ID ) ) || in_category( _x( 'asides', 'asides category slug', 'twentyten' ) )  ) : ?>
-					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-					<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
-						<div class="entry-summary">
-							<?php the_excerpt(); ?>
-						</div><!-- .entry-summary -->
-					<?php else : ?>
-						<div class="entry-content">
-							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
-						</div><!-- .entry-content -->
-					<?php endif; ?>
-
-						<div class="entry-utility">
-							<?php twentyten_posted_on(); ?>
-							<span class="meta-sep">|</span>
-							<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-						</div><!-- .entry-utility -->
-					</div><!-- #post-## -->
-
-			<?php /* How to display all other posts. */ ?>
-
-				<?php else : ?>
 					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php $post_heading_tag = ( is_single() ) ? 'h1' : 'h2'; ?>
-					<<?php echo $post_heading_tag; ?> class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></<?php echo $post_heading_tag; ?>>
+					<<?php echo $post_heading_tag; ?> class="post_title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></<?php echo $post_heading_tag; ?>>
 
+					<?php if ( ! is_single() ) : ?>
 						<div class="entry-meta">
 							<?php
 							printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'twentyten' ),
@@ -198,6 +96,7 @@
 							);
 							?>
 						</div><!-- .entry-meta -->
+				<?php endif; ?>
 
 				<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
 						<div class="entry-summary">
@@ -233,8 +132,6 @@
 
 					<?php comments_template( '', true ); ?>
 
-				<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
-
 			<?php endwhile; // End the loop. Whew. ?>
 
 			<?php /* Display navigation to next/previous pages when applicable */ ?>
@@ -245,34 +142,35 @@
 							</div><!-- #nav-below -->
 			<?php endif; ?>
 			</div><!-- #content -->
-		</div><!-- #container -->
 
-		<aside id="sidebar">
-			<ul class="xoxo">
-			<li id="search" class="widget-container widget_search">
-				<?php get_search_form(); ?>
-			</li>
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
+		<ul id="nav_header" class="g gc5 gfl gmb0 gw" style="position:absolute;top:20px;">
+			<li class="g gc1 gfl gmb0"><a href="http://blog.pearbudget.com">blog home</a></li>
+			<li class="g gc1 gfl gmb0"><a href="https://pearbudget.com/">PearBudget home</a></li>
+			<li class="g gc1 gfl gmb0"><a href="/help">Need Help with PB?</a></li>
+			<li class="g gc1 gfl gmb0"><a href="/contact">Twitter / Facebook</a></li>
+			<li class="g gc1 gfl gmb0"><a href="">Subscribe</a></li>
+		</ul>
+
+		<div id="sidebar" class="g gfl gc2 gmr0">
+			<?php if ( is_single() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<h3>About This Post</h3>
+				<p>This was posted <span style="text-transform:lowercase"><?php wp_days_ago(); ?></span> (on <?php the_date(); ?>), by <?php the_author(); ?>.</p>
+				<?php endwhile; ?>
+			<?php endif; ?>
+			
+			<h3>About Us</h3>
+			<p><a href="https://pearbudget.com/">PearBudget</a> is a really simple budgeting tool. It&rsquo;s perfect for people who know they probably <em>should</em> budget, but who aren&rsquo;t totally sure how to go about it.</p>
+			<p>You can <a href="https://pearbudget.com/">set up a budget for free</a>, and if you want to use it after the free 30-day trial, it&rsquo;s only $4.95 a month &mdash; you&rsquo;ll easily save ten times that much every month.</p>
+
+			<h3>Contact</h3>
+			<p>You can e-mail Charlie (the guy who made PearBudget) or Ruth (our customer service manager), at <a href="mailto:help@pearbudget.com">help@pearbudget.com</a>. You can also send us a note on <a href="http://twitter.com/pearbudget">Twitter</a> or at <a href="http://facebook.com/pearbudget">Facebook</a>.</p>
+
+			<h3>Archives</h3>
+			<ul>
+				<?php wp_get_archives( 'type=monthly' ); ?>
 			</ul>
-		</aside>
-	</div><!-- #main -->
-
-	<div id="footer" role="contentinfo">
-	</div><!-- #footer -->
-
+		</div><!-- #sidebar -->
 
 
 
